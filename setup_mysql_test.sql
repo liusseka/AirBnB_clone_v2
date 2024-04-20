8aef9c5faa89c0fe0bdb7b4354dec3cb7a3fff2e
cjmatheka <cjmatheka@gmail.com>
2024-04-20 08:48:41 +0300

    Add Task 4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- prepares a server for this project
+-- creates db, test user, and grants permissions
+CREATE DATABASE IF hbnb_test_db;
+CREATE USER IF'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance schema . * TO 'hbnb_test'@'localhost';
