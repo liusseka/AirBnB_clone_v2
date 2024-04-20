b0a026ff74ee06914287b4802f8c590b49a58121
cjmatheka <cjmatheka@gmail.com>
2024-04-20 08:40:28 +0300

    Update Task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
---- prepares server for the project
-
-CREATE DATABASE hbnb_dev_db;
+-- prepares server for the project
+-- integrates database, user, and permissions
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
