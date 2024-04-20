25438f4e69caf4d3bec148e4d1e07bdc522463f4
cjmatheka <cjmatheka@gmail.com>
2024-04-20 22:39:49 +0300

    Update Task 7

--- models/user.py
+++ models/user.py
@@ -7,9 +7,10 @@ from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __table__ = "users"
 
-    email = Column(string(128), nullable=False)
-    password = Column(string(128), nullable=False)
-    first_name = Column(string(128), nullable=False)
-    last_name = Column(string(128), nullable=False)
+    __tablename__ = "users"
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
