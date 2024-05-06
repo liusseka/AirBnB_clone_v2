0eebe4d79e7dfa54ff53eedb7ea6b7abdf9758b7
cjmatheka <cjmatheka@gmail.com>
2024-05-06 15:22:10 +0300

    Updat6e Task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,25 +11,26 @@ from datetime import datetime
 from fabric.api import env, local, mkdir, run
 
 def do_pack():
-  """
-  Generates a .tgz archive of web_static contents and returns its path.
-  """
-  now = datetime.utcnow()
-  timestamp = now.strftime('%Y%m%d%H%M%S')
 
-  archive_name = f"web_static_{timestamp}.tgz"
-  archive_path = f"versions/{archive_name}"
+	"""
+  	Generates a .tgz archive of web_static contents and returns its path.
+  	"""
+  	now = datetime.utcnow()
+  	timestamp = now.strftime('%Y%m%d%H%M%S')
 
-  try:
-    run("mkdir -p versions")
-  except Exception as e:
-    print(f"Error creating versions directory: {e}")
-    return None
+	archive_name = f"web_static_{timestamp}.tgz"
+	archive_path = f"versions/{archive_name}"
 
-  with local():
-    try:
-      local(f"tar -czvf {archive_path} web_static/*")
-      return archive_path
-    except Exception as e:
-      print(f"Error creating archive: {e}")
-      return None
+	try:
+		run("mkdir -p versions")
+	except Exception as e:
+		print(f"Error creating versions directory: {e}")
+		return None
+
+	with local():
+		try:
+			local(f"tar -czvf {archive_path} web_static/*")
+			return archive_path
+		except Exception as e:
+			print(f"Error creating archive: {e}")
+			return None
