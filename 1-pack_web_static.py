dd753b2f46f54cf2b9d12cb115a75e86932af5c9
cjmatheka <cjmatheka@gmail.com>
2024-05-06 18:20:49 +0300

    Update Task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,23 +8,20 @@
 from datetime import datetime
 from fabric.api import local
 
-
 def do_pack():
     """Generates a tgz archive from web_static folder."""
+
+    #define time
     now = datetime.utcnow()
     timestamp = now.strftime('%Y%m%d%H%M%S')
+
+    # defile file name and path
+    archive_name = f"web_static_{timestamp}.tgz"
+    archive_path = f"versions/{archive_name}"
 
     # Create versions directory if it doesn't exist
     local("mkdir -p versions")
 
-    # Archive Files
-    archive_name = f"web_static_{timestamp}.tgz"
-    archive_path = os.path.join("versions", archive_name)
-
     # Create archive
-    gzip_file = local(f"tar -czvf {archive_path} web_static/")
-
-    if gzip_file.succeeded:
-        return archived_path
-    else:
-        return None
+    local(f"tar -czvf {archive_path} web_static/")
+    return archive_path
