cb0d78f3ce4fa922a2a5e8ed682ad160e8ac41c9
cjmatheka <cjmatheka@gmail.com>
2024-05-21 23:31:02 +0300

    Update Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,17 +2,17 @@
 """Starts a Flask Web Application"""
 
 from flask import Flask
-app.url_map.strict_slashes = False
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def hello_hbnb:
+def hello_hbnb():
     """Displays Hello hbnb"""
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    app.debug = True
+    app.run(debug=True)
     app.run(host='0.0.0.0', port=5000)
