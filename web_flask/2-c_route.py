ad17af2edd3692ab96eaee159599c4e7d87dbe66
cjmatheka <cjmatheka@gmail.com>
2024-05-21 23:45:44 +0300

    Add Task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Starts a Flask Web Application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """Displays Hello hbnb"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Displays HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """Returns url with a variable"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
